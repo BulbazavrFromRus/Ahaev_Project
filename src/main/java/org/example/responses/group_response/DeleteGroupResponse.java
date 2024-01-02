de3008e20e6417dc7ff0340ecf5f2cae87c95854
Danila <dbulbakov@bk.ru>
2024-01-02 11:18:08 +0600

    I finished it

---
+++ src/main/java/org/example/responses/group_response/DeleteGroupResponse.java
@@ -0,0 +1,9 @@
+package org.example.responses.group_response;
+
+public class DeleteGroupResponse {
+    private long id;
+
+    public DeleteGroupResponse(long id) {
+        this.id = id;
+    }
+}
